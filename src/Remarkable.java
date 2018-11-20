decc4b540392b8382f4faf8ec1a6dd777248e3d8
League <League@AdminisorsiMac2.attlocal.net>
2018-11-19 16:50:29 -0800

    today... >:D
    i did remarkable today it went well plus i started secret message box

---
+++ src/Remarkable.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+public static void main(String[] args) {
+	String mon = JOptionPane.showInputDialog("what is your name?");
+	if (mon.equals("crazyblox"))  {
+		JOptionPane.showMessageDialog(null, "Your REMARKABLE for creating flood escape 2.");}
+		else if (mon.equals("colin"))	{
+				JOptionPane.showMessageDialog(null, "Your remarkable for driving your mom crazy."); }
+				else if 	(mon.equals("henry")) {
+					
+				JOptionPane.showMessageDialog(null, "Your remarkable because you can beat levels in call of duty.");
+			}	
+				else if (mon.equals("charlotte"))	{
+			JOptionPane.showMessageDialog(null, "your remarkable for being good at coding");}
+				else if  (mon.equals("audrey")) {
+	JOptionPane.showMessageDialog(null, "your remarkable for demolishing Henry in volleyball");
+				}
+}
+}
