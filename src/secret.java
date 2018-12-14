eefd25fc8a40835720768fc3d0358529eb05f705
colinizadi <league@WTS21.attlocal.net>
2018-12-13 16:54:36 -0800

    lkololol
    i finished 2

--- src/secret.java
+++ src/secret.java
@@ -1,10 +1,14 @@
 import javax.swing.JOptionPane;
 
 public class secret {
-public static void main(String[] args) {
-	String password = "DaPassword";
-	String rbx = JOptionPane.showInputDialog("tell me a secret message");		
-	String rbx = JOptionPane.showInputDialog("you can only see the secret message if you can guess the password");
-	if(password.equals("DaPassword"))
-}
+	public static void main(String[] args) {
+		String password = ":D :] D: D; :. :/";
+		String rbx = JOptionPane.showInputDialog("tell me a secret message");
+		String smh = JOptionPane.showInputDialog("you can only see the secret message if you can guess the password");
+		if (smh.equals(":D :] D: D; :. :/")) {
+			JOptionPane.showMessageDialog(null, rbx);
+		} else {
+			JOptionPane.showMessageDialog(null, "INTRUDER!!!!!!!!!!!!!");
+		}
+	}
 }
--- src/strings_and_dialogs/riddler.java
+++ src/strings_and_dialogs/riddler.java
@@ -3,8 +3,14 @@ package strings_and_dialogs;
 import javax.swing.JOptionPane;
 
 public class riddler {
-public static void main(String[] args) {
-	int score = 0;
-	JOptionPane.showInputDialog(message)
-}
+	public static void main(String[] args) {
+		int score = 0;
+		String lol = JOptionPane
+				.showInputDialog("what has hands but cant wave. SPELL IN LOWERCASE!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		if (lol.equals("a clock")) {
+			JOptionPane.showMessageDialog(null, "you rock!");
+			score += 2;
+		} else { JOptionPane.showMessageDialog(null, "try again");
+		}
+	}
 }
